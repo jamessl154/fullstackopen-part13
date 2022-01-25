cd51c6faade0986ae8daed97c099c966a05f6ec8
James Sue-Ling <james_sueling@hotmail.co.uk>
2022-01-25 17:36:26 +0000

    refactored errorHandler into switch case statement

--- controllers/errorHandler.js
+++ controllers/errorHandler.js
@@ -1,16 +1,19 @@
 const errorHandler = (err, req, res, next) => {
   console.error(err.message)
 
-  if (err.message === 'Blog not found') {
-    return res.status(404).send({ error: err.message })
-  } else if (err.name === "SequelizeValidationError") {
-    return res.status(400).send({ error: err.message })
-  } else if (err.message === 'Malformatted likes') {
-    return res.status(400).send({ error: "The likes property must be: 1. Formatted as an integer 2. A non-negative integer" })
-  } else if (err.message === 'User not found') {
-    return res.status(404).send({ error: err.message })
-  } else if (err.name === 'SequelizeUniqueConstraintError') {
-    res.status(400).send({ error: err.errors[0].message })
+  switch (err.message) {
+    case 'Blog not found':
+      return res.status(404).send({ error: err.message })
+    case 'Malformatted likes':
+      return res.status(400).send({ error: "The likes property must be: 1. Formatted as an integer 2. A non-negative integer" })
+    case 'User not found':
+      return res.status(404).send({ error: err.message })
+    default:
+      break
+  }
+
+  if (err.name.includes('Sequelize')) {
+    return res.status(400).send({ error: err.errors[0].message })
   }
 
   next(err) // pass forward unhandled errors to the default Express error handler
